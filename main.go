b0f37e0740d4e1245d466d3a8f106f36c8208955
Chris <cmcfee@cryptolab.net>
2018-11-20 18:33:45 +0000

    better idea on how to finish last two requirements

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ package main
 
 import (
 	"bufio"
-	"container/list"
+	//"container/list"
 	"fmt"
 	"io"
 	"log"
@@ -21,9 +21,32 @@ import (
 var ServerName string = "Server"
 var defaultName string = "GuestNNN"
 
-type userList struct {
-	Users map[string]User
-}
+//type userList struct {
+//	Users map[string]User
+
+//package main
+
+//import (
+///		"container/list"
+//			"fmt"
+//		)
+
+//		func main() {
+// Create a new list and put some numbers in it.
+//	l := list.New()
+//		e4 := l.PushBack(4)
+//			e1 := l.PushFront(1)
+//				l.InsertBefore(3, e4)
+//					l.InsertAfter(2, e1)
+
+// Iterate through list and print its contents.
+//							for e := l.Front(); e != nil; e = e.Next() {
+//										fmt.Println(e.Value)
+//											}
+//
+//										}
+
+//}
 
 type Newname struct {
 	Name string
@@ -46,13 +69,34 @@ type Server struct {
 	Input chan Message
 }
 
-func addusertolist() {
+//type UserList struct {
+//Userslice := make([]string, 0, 999)
+//}
 
+func addusertolist(UserSlice []string, addedname string) (newlist []string) {
+	newlist = append(UserSlice, addedname)
+	return newlist
 }
 
-func deluserfromlist() {
+//func deluserfromlist(names, userList, deletedname string) {
 
-}
+//	a := []string{"A", "B", "C", "D", "E"}
+//	i := 2
+//
+// Remove the element at index i from a.
+//	copy(a[i:], a[i+1:]) // Shift a[i+1:] left one index.
+//	a[len(a)-1] = ""     // Erase last element (write zero value).
+//	a = a[:len(a)-1]     // Truncate slice.
+
+//	fmt.Println(a) // [A B D E]
+
+//names.Userslice = append(names.Userslice[:i
+///
+//	a = append(a[:i], a[i+1:]...)
+// or
+//	a = a[:i+copy(a[i:], a[i+1:])]
+//
+//}
 
 func listallusers(Users map[string]User) (listofusers_result string) {
 	//userlist = userlist + cur_list
@@ -110,7 +154,7 @@ func (srvr *Server) Run() {
 	}
 }
 
-func handleConn(srvr *Server, conn net.Conn, Users map[string]User, userlist string) {
+func handleConn(srvr *Server, conn net.Conn, Users map[string]User, UserSlice []string) {
 	defer conn.Close()
 	guest := "Guest"
 	user := User{
@@ -135,15 +179,18 @@ func handleConn(srvr *Server, conn net.Conn, Users map[string]User, userlist str
 		for scanner.Scan() {
 			ln := scanner.Text()
 			nickprefix := `/nick`
-			newUser := user.Name
-			userlist = userlist + newUser
+			//newUser := user.Name
+			//addedname = user.Name
+			UserSlice = addusertolist(UserSlice, user.Name)
+			//userlist = userlist + newUser
 			if strings.HasPrefix(ln, nickprefix) {
 				nn := changeNick(ln, nickprefix)
 				io.WriteString(conn, "Changed nickname. ")
 				io.WriteString(conn, "Nickname changed to: ")
 				io.WriteString(conn, nn)
-				user.Name = nn
-				userlist = userlist + user.Name
+				UserSlice = addusertolist(UserSlice, nn)
+				//user.Name = nn
+				//userlist = userlist + user.Name
 				//io.WriteString(conn, userlist)
 				//listofusers = listallusers(Users, userlist, nn)
 				//io.WriteString(conn, listofusers)
@@ -161,8 +208,17 @@ func handleConn(srvr *Server, conn net.Conn, Users map[string]User, userlist str
 			}
 		}
 	}()
-	io.WriteString(conn, userlist)
+	//io.WriteString(conn, userlist)
 	//write to connection
+	//for i := 0; i < len(UserSlice); i++ { //looping from 0 to the length of the array
+	//	fmt.Printf("%d th element of UsersSlice is %.2f\n", i, UserSlice[i])
+	//}
+
+	for index, each := range UserSlice {
+		fmt.Printf("value [%d] is [%s]\n", index, each)
+	}
+
+	//for _, UserSlice
 	for msg := range user.Output {
 		if msg.Nickname != user.Name {
 			_, err := io.WriteString(conn, msg.Nickname+": "+msg.Msgtext+"\n")
@@ -203,6 +259,9 @@ func main() {
 		Input: make(chan Message),
 	}
 
+	UserSlice := make([]string, 0, 999)
+	UserSlice = append(UserSlice, "Users Online: ")
+
 	//nickList := &userList{
 	//	Users:
 	//var listofuserst string
@@ -210,14 +269,16 @@ func main() {
 
 	go mainServer.Run()
 
-	userlist := "Users online: "
-	usersonline := userlist + listallusers(mainServer.Users)
+	//userlist := "Users online: "
+	//usersonline := userlist + listallusers(mainServer.Users)
+
+	//userlist := make([]string, len(s))
 
 	for {
 		conn, err := server.Accept()
 		if err != nil {
 			log.Fatalln(err.Error())
 		}
-		go handleConn(mainServer, conn, mainServer.Users, usersonline)
+		go handleConn(mainServer, conn, mainServer.Users, UserSlice)
 	}
 }
